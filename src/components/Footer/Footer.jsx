28c674e28ea88b9bac00c437a651e8eff56bca83
Rupam-B <rupam.banerje@gmail.com>
2024-01-06 11:20:10 +0530

    sidebar modified 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./FooterStyle.css";
+import { useSelector } from 'react-redux';
 
 const Footer = () => {
+
+  const sidebarData = useSelector((state)=>state.OpenCloseSidebar)
   return (
-    <div className="Footter-main-div">
+    <div className={sidebarData===1?"Footter-main-div-Enhance":"Footter-main-div"}>
       <div className="row Footer-top">
         <div className="col-md-8 Footter-sub-top-div-one">
           <h5>BE THE FIRST TO KNOW</h5>
@@ -13,7 +16,7 @@ const Footer = () => {
             <button>Subscribe</button>
           </div>
         </div>
-        <div className="col-md-4 Footter-sub-top-div-two">
+        <div style={{height:'25vh'}} className="col-md-4 Footter-sub-top-div-two">
           <h5>CONTACT US</h5>
           <span>9876543210</span>
           <span>Customer@BrandInfinity.com</span>
@@ -22,14 +25,15 @@ const Footer = () => {
           {/* <hr className="contact-us-mobile-footer-line" /> */}
         </div>
       </div>
-      <div className="row Footer-bottom">
-        <div className="col-md-4 Footter-sub-bottom-div-one">
+      <div  className="row Footer-bottom">
+        <div  className="col-md-4 Footter-sub-bottom-div-one">
           <h5>meta muse</h5>
           <p>About us</p>
           <p>Stories</p>
           <p>Articians</p>
           <p>Boutiqes</p>
           <p>Contact Us</p>
+          <i className="fa-solid fa-angle-down hide-angle-down-large"></i>
         </div>
         <div className="col-md-4 Footter-sub-bottom-div-two">
           <h5>QuickLinks</h5>
@@ -39,6 +43,8 @@ const Footer = () => {
           <p>Return</p>
           <p>FAQs</p>
           <p>Privacy Policy</p>
+          <i className="fa-solid fa-angle-down hide-angle-down-large"></i>
+
         </div>
         <div className="col-md-4 Footter-sub-bottom-div-three">
           <h5>Follow Us</h5>
